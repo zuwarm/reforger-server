115b9574c9f888ee0c701d7092949a05c48874af
zuwarm <c.schoene@web.de>
2023-03-15 13:27:06 +0100

    Changed Docker volume path

--- new_reforger_container.sh
+++ new_reforger_container.sh
@@ -2,5 +2,5 @@
 docker create \
         --name=reforger-server \
         -p 2001:2001/udp \
-        -v /home/anti/reforger/configs:/reforger/Configs \
+        -v $HOME/reforger/configs:/reforger/Configs \
         reforger:test
